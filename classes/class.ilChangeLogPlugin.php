8c0628ca45d8c5c907be481a9077277648d584b4
Florian <fw@studer-raimann.ch>
2018-10-08 08:13:02 +0200

    Remove todo

--- classes/class.ilChangeLogPlugin.php
+++ classes/class.ilChangeLogPlugin.php
@@ -152,7 +152,7 @@ class ilChangeLogPlugin extends ilEventHookPlugin {
 					case 'afterCreate':
 						$this->changeLog->handleCreation($component, $parameters);
 						break;
-					case 'beforeUpdate': //@todo @fwyss see Services/User/classes/class.ilObjUser.php:554
+					case 'beforeUpdate':
 						$this->changeLog->handleModification($component, $parameters);
 						break;
 					case 'deleteUser':
