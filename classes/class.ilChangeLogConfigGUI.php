af3e62962a8ed4beb0d08d55378ee61ff7e46287
Florian Wyss <fw@studer-raimann.ch>
2019-03-22 12:42:03 +0100

    Reduce require's

--- classes/class.ilChangeLogConfigGUI.php
+++ classes/class.ilChangeLogConfigGUI.php
@@ -1,8 +1,6 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
-require_once __DIR__ . "/../vendor/autoload.php";
-
 use srag\ActiveRecordConfig\ChangeLog\ActiveRecordConfigGUI;
 use srag\Plugins\ChangeLog\Config\ChangeLogConfigFormGUI;
 use srag\Plugins\ChangeLog\Utils\ChangeLogTrait;
